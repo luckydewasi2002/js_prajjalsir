1d6eb110f2f26f35287cf2a1efb7a4a9d2e9ef8b
laxmanraika2002@gmail.com <laxmanraika2002@gmail.com>
2024-06-29 18:00:20 +0530

    updated

--- new.js
+++ new.js
@@ -1,27 +1,7 @@
-const vehicle = {
-    vehicleModel: 'Tesla Model 3',
-    vehiclePrice: 49999,
-    vehicleColor: 'Midnight Silver Metallic',
-    vehicleFeatures: ['Autopilot', 'Long Range Battery', 'Premium Interior'],
-    vehicleSpecs: {
-        horsepower: 283,
-        torque: '273 lb-ft',
-        topSpeed: '140 mph'
-    }
-};
-
-
-function function2 (vehicleFeatures) {
-    vehicleFeatures.forEach((vehicleFeatures) => {
-        console.log(vehicleFeatures);
-    });
+function findmin(...nums){
+    
+let min = 0;
+    nums.reduce((min,nums)=>min<nums?nums:min);
 }
+console.log(findmin(1,2,3,4,5,6))
 
-
-function printVehicleInfo({vehicleModel,vehiclePrice,vehicleColor,vehicleFeatures,vehicleSpecs}){
-    console.log(`my vehicle  model is :${vehicleModel} and my vehicle price is :${vehiclePrice} and vehicle color is :
-         ${vehicleColor} and vehicle features is :${vehicleFeatures} and vehicles specs is :${vehicleSpecs}`)
-              function2(vehicleFeatures)
-        }
-
-printVehicleInfo(vehicle);
