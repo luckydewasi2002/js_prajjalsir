9edb5b2709d756791315b2107522682b2354c280
laxmanraika2002@gmail.com <laxmanraika2002@gmail.com>
2024-07-15 10:43:21 +0530

    updated

--- new.js
+++ new.js
@@ -1,27 +1,62 @@
-fetch( ' https://jsonplaceholder.typicode.com/users')
-  .then(response => response.json())  
-  .then(data => {
-   
+//13. 
+// async function fetchdata() {
+//   try {
+      
+//       const response = await fetch('https://jsonplaceholder.typicode.com/users');
+//       const data = await response.json(); 
+
+//      //console.log(data)
+//      const fullAddressUsers = users.map(user => {
+//       const { street, suite, city, zipcode } = user.address;
+//       return `${street}, ${suite}, ${city}, ${zipcode}`;
+//     });
+//     console.log('Users with full address strings:', fullAddressUsers);
+
+ 
+//   } catch (error) {
+     
+//       console.error( error);
+//   }
+// }
   
-    const fieldsToInclude = ['field1', 'field2', 'field3'];
 
-    // Create a new array with only the specified fields
-    const filteredData = data.map(item => {
-      // Create a new object with only the specified fields
-      const filteredItem = {};
-      fieldsToInclude.forEach(field => {
-        if (item.hasOwnProperty(field)) {
-          filteredItem[field] = item[field];
-        }
-      });
-      return filteredItem;
-    });
+// fetchdata();
 
-    // Log the new array
-    console.log('Filtered Data:', filteredData);
-  
-  })
-  .catch(error => {
-    console.error( error);
-   });
+//
+// 9)Data fetch hone ke baad, users ka name ka ek naya array banao using reduce method.
 
+// async function fetchAndProcessUsers() {
+//   try {
+//     const response = await fetch('https://jsonplaceholder.typicode.com/users');
+//     const users = await response.json();
+
+//     const userNames = users.reduce((names, user) => {
+//       names.push(user.name);
+//       return names;
+//     }, []);
+//     console.log('User names:', userNames);
+
+// }catch (error) {
+//   console.error('Error fetching users:', error);
+// }
+// }
+// fetchAndProcessUsers();
+// 10)Data fetch hone ke baad, users ka ek naya array banao jo users ke email contain karta ho
+//  jin ka address.zipcode "92998-3874" hai using filter and map methods.
+
+
+// async function fetchAndProcessUsers() {
+//   try {
+//     const response = await fetch('https://jsonplaceholder.typicode.com/users');
+//     const users = await response.json();
+
+//     const specificZipEmails = users
+//       .filter(user => user.address.zipcode === "92998-3874")
+//       .map(user => user.email);
+//     console.log('Emails of users with zipcode 92998-3874:', specificZipEmails);
+
+// }catch (error) {
+//   console.error('Error fetching users:', error);
+// }
+// }
+// fetchAndProcessUsers();
